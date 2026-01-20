7bbf80972ec9b9483e36b0599f99d11ce8cb61f8
GoRankeds Dev <gorankedsdev@gmail.com>
2026-01-20 14:55:06 +0000

    Initial prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <></>;
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-5xl font-bold text-primary">Hola Mundo</h1>
+    </main>
+  );
 }
