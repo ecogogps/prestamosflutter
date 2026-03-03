2d4b43b176e71f084d050a4e963b7eb58cd8d5cd
GoRankeds Dev <gorankedsdev@gmail.com>
2026-03-03 18:40:54 +0000

    cambia el verde por este 71AF57 SOLO ESO

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color background = Color(0xFF212529);
-  static const Color primary = Color(0xFF8BF724);
+  static const Color primary = Color(0xFF71AF57);
   static const Color text = Color(0xFFFFFFFF);
-  static const Color surface = Color(0xFF2C3136);
-  static const Color error = Color(0xFFE74C3C);
+  static const Color cardBackground = Color(0xFF2C3136);
 }
